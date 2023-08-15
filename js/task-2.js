6a5923bd0e0f0d70d94ae8f638b78b6909b08530
MeriCheri <chernovol.maria@gmail.com>
2023-08-15 14:40:51 +0300

    first commit

---
+++ js/task-2.js
@@ -0,0 +1,27 @@
+
+// 1
+const btnEl = document.querySelector(".button");
+// btnEl.textContent = "нове імя";
+// console.log(btnEl.textContent);
+
+// 2
+
+const imgEl = document.querySelector(".img");
+// imgEl.src = "https://images.squarespace-cdn.com/content/v1/51f17990e4b0bef09d514799/1512679153861-LA2E5A4XV0VMCMWDA0QN/bartlett.jpg?format=1500w"
+// console.log(imgEl)
+
+// 3
+
+const linkEl = document.querySelector(".link");
+// linkEl.href = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/800px-Banana-Single.jpg"
+// console.log(linkEl.href);
+
+const img2El = document.querySelector(".img2");
+// img2El.alt = "newAlt"
+// console.log(img2El);
+
+// 4
+const listEl = document.querySelector(".list");
+// const firstItem = listEl.firstElementChild
+// firstItem.textContent = "sadasd"
+// console.log(firstItem?.textContent)
